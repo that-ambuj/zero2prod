88664270ee02c1d83f17cebe5bec53d2400e6d18
that-ambuj <ambujs89@gmail.com>
2022-12-07 17:20:50 +0530

    randoming database in testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 
 pub struct TestApp {
     pub address: String,
@@ -13,11 +14,10 @@ async fn spawn_app() -> TestApp {
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("failed to read config");
-    let connection_pool =
-        PgPool::connect(&configuration.database.connection_string())
-            .await
-            .expect("failed to connect to postgres");
+    let mut configuration = get_configuration().expect("failed to read config");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
 
     let server = zero2prod::run(listener, connection_pool.clone())
         .expect("Failed to bind address");
@@ -28,6 +28,29 @@ async fn spawn_app() -> TestApp {
         address,
         db_pool: connection_pool,
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db())
+            .await
+            .expect("failed to connect to postgres");
+    connection
+        .execute(
+            format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str(),
+        )
+        .await
+        .expect("failed to create database.");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("failed to connect to postgres");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("failed to run migrations");
+
+    connection_pool
 }
 
 #[tokio::test]
